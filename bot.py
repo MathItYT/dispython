ee57959ee341b2e831a45fc260de01d3261b5b0e
MathLike <thebenjitaubilla@gmail.com>
2023-03-20 23:15:19 -0300

    Fix

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def python(ctx: commands.Context):
                 bot_msg = await channel.send(to_send)
         else:
             if to_send.strip() != "":
-                bot_msg = await bot_msg.edit(content=bot_msg.content + "\n" + line)
+                bot_msg = await bot_msg.edit(content=bot_msg.content + "\n" + to_send)
     os.remove(filename)
 
 
